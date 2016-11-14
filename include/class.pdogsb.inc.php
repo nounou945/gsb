33e382612643c0875051eaf37202820fdd297cc3
Stéphane <Stphane@10.20.129.64>
2016-11-14 14:29:45 +0100

    changement pdf

--- include/class.pdogsb.inc.php
+++ include/class.pdogsb.inc.php
@@ -12,7 +12,7 @@
  * @package default
  * @author Cheri Bibi
  * @version    1.0
- * @link       http://www.php.net/manual/fr/book.pdo.php
+ * @link       http://www.php.net/manual/fr/book.pdo.php 
  */
 
 class PdoGsb{   		
